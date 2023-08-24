4405c7fd22ca9b847ab2a42397303fc1a25a2d81
Apple <trinq110902@gmail.com>
2023-08-24 16:24:54 +0700

    update ui

--- app/uploads/page.js
+++ app/uploads/page.js
@@ -12,7 +12,16 @@ const Home = () => {
       <div className="container-lg mx-auto">
         <section className="text-center py-20 space-y-4">
           <p className="text-md">Shop At CHUS</p>
-          <p className="text-3xl">PLAY WITH YOUR IMAGE</p>
+          <div className="flex flex-col md:flex-row  items-center justify-center mt-0">
+            <p className="text-[40px] mt-2 ml-[-35%] sm:ml-0">Play With</p>
+            <span className="font-bold relative text-[60px] md:text-[50px] ">
+              Your Image
+              <img
+                className="w-6 self-start absolute top-0 right-[-20px]"
+                src="hat.png"
+              />
+            </span>
+          </div>
         </section>
         <section className="mt-4">
           <div>
@@ -23,7 +32,7 @@ const Home = () => {
           <button
             type="button"
             className="text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-            Upload Image asd asd as
+            Upload Image
           </button>
         </section>
       </div>
