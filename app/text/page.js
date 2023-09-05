8bd069318330254872ee75e73ca79343f394002b
Apple <trinq110902@gmail.com>
2023-09-05 21:48:29 +0700

    asd

--- app/text/page.js
+++ app/text/page.js
@@ -41,6 +41,8 @@ const TextDetect = () => {
       .toJpeg(document.getElementById("ImageDownload"), {
         quality: 1,
         pixelRatio: 1,
+        canvasHeight: window.innerWidth,
+        canvasWidth: window.innerWidth,
       })
       .then(async (dataUrl) => {
         var link = document.createElement("a");
@@ -65,42 +67,6 @@ const TextDetect = () => {
       );
     downloadImg();
   };
-  useEffect(() => {
-    var $objHead = $("head");
-
-    // define a function to disable zooming
-    var zoomDisable = function () {
-      $objHead.find("meta[name=viewport]").remove();
-      $objHead.prepend(`<meta name="viewport" 
-        content="width=device-width, initial-scale=1.0, user-scalable=0" />`);
-    };
-
-    // ... and another to re-enable it
-    var zoomEnable = function () {
-      $objHead.find("meta[name=viewport]").remove();
-      $objHead.prepend(`<meta name="viewport" 
-        content="width=device-width, initial-scale=1.0, user-scalable=1" />`);
-    };
-
-    // if the device is an iProduct, apply the fix whenever the users touches an input
-    if (
-      navigator.userAgent.length &&
-      /iPhone|iPad|iPod/i.test(navigator.userAgent)
-    ) {
-      // define as many target fields as your like
-      $(".search-form input, .contact-form input")
-        .on({
-          touchstart: function () {
-            zoomDisable();
-          },
-        })
-        .on({
-          touchend: function () {
-            setTimeout(zoomEnable, 500);
-          },
-        });
-    }
-  }, []);
   return (
     <>
       {/* zuno added */}
@@ -210,7 +176,7 @@ const TextDetect = () => {
                   {name}
                 </span>
               )}
-              <p className="text-white -mt-4 md:-mt-2">
+              <p className="text-white -mt-4 md:-mt-0">
                 Craft with love, Shop with taste
               </p>
             </div>
