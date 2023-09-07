239da82d7532b2451b01bbe9f973915fb75571aa
zunohan <pnlan1406@gmail.com>
2023-09-07 14:13:11 +0700

    sep tri

--- app/uploads/Last.js
+++ app/uploads/Last.js
@@ -12,14 +12,15 @@ async function sleep(s) {
 const Last = ({ img }) => {
   const t = localStorage.getItem("img") ?? "demo.jpg";
   const [fImg, setFImg] = useState(t);
-  const refImage = useRef();
+  const refPcImage = useRef(null);
+  const refMbImage = useRef(null);
+
   const [isDownload, setIsDownload] = useState(false);
 
   useEffect(() => {
-    const finalImgDom = document.getElementById("final-image");
+    let finalImgDom = document.getElementById("final-image");
     const x = new Cropper(finalImgDom, {
       dragMode: "crop",
-
       autoCrop: true,
       viewMode: 3,
       center: true,
@@ -32,19 +33,36 @@ const Last = ({ img }) => {
       },
       crop(e) {
         const timer = setTimeout(() => {
-          refImage.current.src = x.getCroppedCanvas().toDataURL("image/jpeg");
+          refPcImage.current.src = x.getCroppedCanvas().toDataURL("image/jpeg");
+          refMbImage.current.src = x.getCroppedCanvas().toDataURL("image/jpeg")
           return () => clearTimeout(timer);
         }, 200);
         return () => clearTimeout(timer);
       },
     });
   }, []);
-
-  const downloadImg = async () => {
-      html2canvas(document.getElementById("pre-image-downloads").getElementsByTagName("img")[0], {}).then(
+  if (typeof window !== "undefined") {
+    window.mobileAndTabletCheck = function () {
+      let check = false;
+      (function (a) {
+        if (
+          /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+            a
+          ) ||
+          /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+            a.substr(0, 4)
+          )
+        )
+          check = true;
+      })(navigator.userAgent || navigator.vendor || window.opera);
+      return check;
+    };
+  }
+  const downloadPcImg = async () => {
+      html2canvas(document.getElementById("pre-image-downloads-pc").getElementsByTagName("img")[0], {}).then(
        (canvas) => {
         let cvs = document.createElement("canvas").appendChild(canvas);
-        document.body.appendChild(canvas)
+
         let link = document.createElement("a");
         link.download = "my-upload-img";
         link.href = cvs.toDataURL();
@@ -52,6 +70,19 @@ const Last = ({ img }) => {
       }
     );
   };
+
+  const downloadMbImg = async () => {
+    html2canvas(document.getElementById("pre-image-downloads-mb").getElementsByTagName("img")[0], {}).then(
+     (canvas) => {
+      let cvs = document.createElement("canvas").appendChild(canvas);
+
+      let link = document.createElement("a");
+      link.download = "my-upload-img";
+      link.href = cvs.toDataURL();
+      link.click();
+    }
+  );
+};
 
   return (
     <>
@@ -86,15 +117,15 @@ const Last = ({ img }) => {
           </div>
         </section>
         <section className="grid grid-cols-12">
-          <div className="md:hidden col-span-6 md:col-span-4 flex flex-col justify-end">
+          <div id="pre-image-downloads-mb" className="md:hidden col-span-6 md:col-span-4 flex flex-col justify-end">
             <img
-              ref={refImage}
+              ref={refMbImage}
               className="flex flex-col justify-center items-center mx-auto box-content w-[20vh] h-[20vh] md:w-[21vw] md:h-[21vw] rounded-xl shadow bg-nguyen gap-4"
             />
           </div>
           <section className="md:hidden col-span-5 flex flex-col justify-end text-center space-y-4">
             <button
-            onClick={downloadImg}
+              onClick={downloadMbImg}
               type="button"
               className="w-full text-white font-bold bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
@@ -118,18 +149,17 @@ const Last = ({ img }) => {
             </div>
           </div>
           <div
-            id="pre-image-downloads"
+            id="pre-image-downloads-pc"
             className="hidden md:flex col-span-6 md:col-span-4 flex-col justify-end">
             <img
-
-              ref={refImage}
+              ref={refPcImage}
               className="flex flex-col justify-center items-center mx-auto box-content w-[20vh] h-[20vh] md:w-[21vw] md:h-[21vw] rounded-xl shadow bg-nguyen gap-4"
             />
           </div>
         </section>
         <section className="hidden sm:block text-center py-10 space-x-10">
           <button
-          onClick={downloadImg}
+            onClick={downloadPcImg}
             type="button"
             className="text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
