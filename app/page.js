e3e1011607429e45c2da4931d3cee203532fbd81
zuno90 <pnlan1406@gmail.com>
2023-09-28 11:35:49 +0700

    jjjjffff

--- app/page.js
+++ app/page.js
@@ -19,14 +19,28 @@ export default function Home() {
     }
   }, [isChangedLang]);
 
-  // if (typeof window !== "undefined") {
-  //   if (window.navigator.userAgent.indexOf("cordova") !== -1) {
-  //     // do something here
-  //     window.open(window.location.href, "_system");
-  //   } else {
-  //     console.log("ko phai webapp", window.location.href);
-  //   }
-  // }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const isWebview = () => {
+        const navigator = window.navigator;
+        const userAgent = navigator.userAgent;
+        const normalizedUserAgent = userAgent.toLowerCase();
+        const standalone = navigator.standalone;
+
+        const isIos =
+          /ip(ad|hone|od)/.test(normalizedUserAgent) ||
+          (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
+        const isAndroid = /android/.test(normalizedUserAgent);
+        const isSafari = /safari/.test(normalizedUserAgent);
+        const isWebview =
+          (isAndroid && /; wv\)/.test(normalizedUserAgent)) ||
+          (isIos && !standalone && !isSafari);
+
+        return isWebview;
+      };
+      if (isWebview) window.alert("Trên webview");
+    }
+  }, []);
 
   return (
     <>
